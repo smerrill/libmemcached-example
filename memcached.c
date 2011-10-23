941636f1067172e8b40f90a15d3c01336c8b3785
Steven Merrill <steven.merrill@gmail.com>
2011-10-23 16:15:58 -0700

    Working libmemcached example.

---
+++ memcached.c
@@ -0,0 +1,30 @@
+#include <libmemcached/memcached.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  //memcached_servers_parse (char *server_strings);
+  memcached_server_st *servers = NULL;
+  memcached_st *memc;
+  memcached_return rc;
+  char *key = "keystring";
+  char *value = "keyvalue";
+
+  memc = memcached_create(NULL);
+  servers = memcached_server_list_append(servers, "localhost", 11211, &rc);
+  rc = memcached_server_push(memc, servers);
+
+  if (rc == MEMCACHED_SUCCESS)
+    fprintf(stderr, "Added server successfully\n");
+  else
+    fprintf(stderr, "Couldn't add server: %s\n", memcached_strerror(memc, rc));
+
+  rc = memcached_set(memc, key, strlen(key), value, strlen(value), (time_t)0, (uint32_t)0);
+
+  if (rc == MEMCACHED_SUCCESS)
+    fprintf(stderr, "Key stored successfully\n");
+  else
+    fprintf(stderr, "Couldn't store key: %s\n", memcached_strerror(memc, rc));
+
+  return 0;
+}
